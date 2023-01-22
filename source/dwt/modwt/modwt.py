Test reconstruct short finds the correct values, just in the wrong order

source/dwt/modwt/modwt.py
@@ -40,7 +40,47 @@ def circular_convolve_mra( signal, ker ):
         Modification of 
             https://stackoverflow.com/questions/35474078/python-1d-array-circular-convolution
     '''
-    return np.flip(np.real(np.fft.ifft(np.fft.fft(signal)*np.fft.fft(np.flip(ker))))).astype(np.int64).tolist()
+
+    # check if signal and ker have same shape
+    
+    assert signal.shape == ker.shape
+
+    fSignal = np.fft.fft(signal)
+    fKer = np.fft.fft(np.flip(ker))
+    # fKer = np.fft.fft(ker)
+    fConv = fSignal * fKer
+    conv = np.real(np.fft.ifft(fConv))
+    conv = np.flip(conv)
+    asList = np.flip(conv).tolist()
+    # asList = np.flip(conv).astype(np.int64).tolist()
+    # asList = conv.astype(np.int64).tolist()
+    return asList
+    
+    # return np.flip(np.real(np.fft.ifft(np.fft.fft(signal)*np.fft.fft(np.flip(ker))))).astype(np.int64).tolist()
+
+
+def circular_convolve_no_pad(x, y):
+    x_len = x.shape[0]
+    y_len = y.shape[0]
+    conv = np.zeros(x_len)
+    for n in range(x_len):
+        for m in range(y_len):
+            # print(f"n={n}, m={m}, (n-m)={(n-m)}, (n-m) % y_len={(n-m) % y_len}")
+            conv[n] += x[m] * y[(n-m) % y_len]
+    asList = conv.tolist()
+    return asList
+
+    
+def circular_convolve_no_pad_int(x, y):
+    x_len = x.shape[0]
+    y_len = y.shape[0]
+    conv = np.zeros(x_len)
+    for n in range(x_len):
+        for m in range(y_len):
+            # print(f"n={n}, m={m}, (n-m)={(n-m)}, (n-m) % y_len={(n-m) % y_len}")
+            conv[n] += x[m] * y[(n-m) % y_len]
+    asList = conv.astype(np.int64).tolist()
+    return asList
 
 
 def circular_convolve_d(h_t, v_j_1, j):

source/dwt/test/test_dwt.py
@@ -28,6 +28,8 @@ def fluct_head(params, x, t):
 # Return tuple with attributes:
 #   time_series
 #   levels
+
+
 def get_fabricated_observations_long():
     levels = 9
     t = linspace(0, 31, 2**levels)
@@ -52,26 +54,53 @@ def get_fabricated_observations_long():
 
     return (time_series, levels)
 
+
 def get_fabricated_observations_short():
     levels = 3
     time_series = np.arange(10)
 
     return (time_series, levels)
 
+
 def reconstruct_signal(mra_series):
     return sum(mra_series, axis=0)
 
 
 class TestDwt(unittest.TestCase):
-    
+
     # make sure that the test framework is working
     def test_framework(self):
         print("test_framework")
 
+    def test_circular_convolve_no_pad(self):
+        x = np.array([1, 2, 3])
+        y = np.array([0, 1, 0.5])
+        expected_output = [4., 2.5, 2.5]
+        res = circular_convolve_no_pad(x, y)
+        print("expected_output: ", expected_output)
+        print("res: ", res)
+        self.assertEqual(res, expected_output)
 
-    
+    def test_circular_convolve_no_pad_int(self):
+        x = np.array([1, 2, 3])
+        y = np.array([0, 1, 0.5])
+        expected_output = [4, 2, 2]
+        res = circular_convolve_no_pad_int(x, y)
+        print("expected_output: ", expected_output)
+        print("res: ", res)
+        self.assertEqual(res, expected_output)
 
-    # make sure that the functions run without error
+    def test_circular_convolve_mra(self):
+        x = np.array([1, 2, 3])
+        y = np.array([0, 1, 0.5])
+        expected_output = circular_convolve_no_pad_int(x, y)
+        res = circular_convolve_mra(x, y)
+        print("expected_output: ", expected_output)
+        print("res: ", res)
+        self.assertEqual(res, expected_output)
+
+
+
     def test_dwt_short(self):
         print("test_dwt_short")
         time_series, levels = get_fabricated_observations_short()
@@ -80,9 +109,6 @@ class TestDwt(unittest.TestCase):
         mra_series = modwtmra(dwt_series, 'db2')
         print("mra_series: ", mra_series)
         print(sum(mra_series, axis=0))
-
-
-
 
     def test_dwt_short_nonzero(self):
         print("test_dwt_short_nonzero")
@@ -112,9 +138,8 @@ class TestDwt(unittest.TestCase):
         print(reconstructed)
         self.assertTrue(reconstructed.any())
 
-    
-
     # check if the reconstructed time series is the same as the original
+
     def test_reconstruct_short(self):
         print("test_reconstruct_short")
         time_series, levels = get_fabricated_observations_short()
@@ -132,10 +157,8 @@ class TestDwt(unittest.TestCase):
         # make sure the reconstructed series is the same as the original
         self.assertTrue(np.array_equal(time_series, reconstructed))
 
-
-
-
     # check if the reconstructed time series is the same as the original
+
     def test_reconstruct_long(self):
         print("test_reconstruct_long")
         time_series, levels = get_fabricated_observations_long()
@@ -143,7 +166,7 @@ class TestDwt(unittest.TestCase):
         dwt_series = modwt(time_series, 'haar', levels)
         mra_series = modwtmra(dwt_series, 'haar')
         print("mra_series: ", mra_series)
-        
+
         reconstructed = reconstruct_signal(mra_series)
 
         # make sure at least one element of mra_series is nonzero